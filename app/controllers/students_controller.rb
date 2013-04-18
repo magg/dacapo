4c75ff3a52b0ed7ce2e3f44f5d22b2b7d7602ad3
Miguel Gonzalez <maggonzz@gmail.com>
2013-04-18 17:32:35 -0500

    first pagination attempt in students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
   # GET /students
   # GET /students.json
   def index
-    @students = Student.all
+    @students = Student.order("created_at DESC").page(params[:page]).per(1)
 
     respond_to do |format|
       format.html # index.html.erb
--- db/migrate/20130301213555_change_matreq_in_subjects.rb
+++ db/migrate/20130301213555_change_matreq_in_subjects.rb
@@ -1,11 +1,11 @@
 class ChangeMatreqInSubjects < ActiveRecord::Migration
   def up
-    #change_column :subjects, :matreq, :integer
-    connection.execute(%q{
-        alter table subjects
-        alter column matreq
-        type integer using cast(matreq as integer)
-      })
+    change_column :subjects, :matreq, :integer
+    #connection.execute(%q{
+     #   alter table subjects
+      #  alter column matreq
+       # type integer using cast(matreq as integer)
+      #})
   end
 
   def down
